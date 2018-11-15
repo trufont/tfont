7038fb51442598e1aed5c21161c8bd7b611b8a17
Nikolaus Waxweiler <madigens@gmail.com>
2018-11-15 00:32:49 +0000

    Convert all layers of a UFO

--- src/tfont/converters/ufoConverter.py
+++ src/tfont/converters/ufoConverter.py
@@ -5,10 +5,12 @@ from tfont.objects.component import Component
 from tfont.objects.feature import FeatureHeader
 from tfont.objects.font import Font
 from tfont.objects.glyph import Glyph
+from tfont.objects.layer import Layer
 from tfont.objects.guideline import Guideline
 from tfont.objects.misc import AlignmentZone, Transformation
 from tfont.objects.path import Path
 from typing import Union
+
 try:
     import ufoLib2
 except ImportError:
@@ -23,7 +25,7 @@ class UFOConverter(cattr.Converter):
             raise ImportError(
                 "No module named 'ufoLib2'. This is required to use the "
                 "UFOConverter. Please re-install tfont with 'ufo' extra:\n"
-                "    $ pip install tfont[ufo]",
+                "    $ pip install tfont[ufo]"
             )
 
         super().__init__(**kwargs)
@@ -38,7 +40,8 @@ class UFOConverter(cattr.Converter):
         if info.openTypeHeadCreated:
             try:
                 font.date = datetime.strptime(
-                    info.openTypeHeadCreated, "%Y/%m/%d %H:%M:%S")
+                    info.openTypeHeadCreated, "%Y/%m/%d %H:%M:%S"
+                )
             except ValueError:
                 pass
         if info.familyName:
@@ -70,7 +73,7 @@ class UFOConverter(cattr.Converter):
             master.name = info.styleName
         for blues in (info.postscriptBlueValues, info.postscriptOtherBlues):
             for yMin, yMax in zip(blues[::2], blues[1::2]):
-                master.alignmentZones.append(AlignmentZone(yMin, yMax-yMin))
+                master.alignmentZones.append(AlignmentZone(yMin, yMax - yMin))
         if info.postscriptStemSnapH:
             master.hStems = info.postscriptStemSnapH
         if info.postscriptStemSnapV:
@@ -104,68 +107,89 @@ class UFOConverter(cattr.Converter):
         # glyphs
         font._glyphs.clear()
         glyphs = font.glyphs
-        for g in ufo:
-            glyph = Glyph(g.name)
+        for glyph_name in ufo.glyphOrder:
+            glyph = Glyph(glyph_name)
             glyphs.append(glyph)
-            if g.unicodes:
-                glyph.unicodes = ["%04X" % uniValue for uniValue in g.unicodes]
             # TODO assign kerning groups
-            # layer
-            layer = glyph.layerForMaster(None)
-            layer.width = g.width
-            layer.height = g.height
-            lib = g.lib
-            vertOrigin = lib.pop("public.verticalOrigin", None)
-            if vertOrigin:
-                layer.yOrigin = vertOrigin
-            if lib:
-                layer._extraData = lib
-            # anchors
-            anchors = layer.anchors
-            for a in g.anchors:
-                if not a.name:
+
+            for ufo_layer in ufo.layers:
+                # Layer.
+                if glyph_name not in ufo_layer:
                     continue
-                anchors[a.name] = Anchor(a.x or 0, a.y or 0)
-                # ufo color and identifier are skipped
-            # components
-            components = layer.components
-            for c in g.components:
-                component = Component(c.baseGlyph)
-                if c.transformation:
-                    component.transformation = Transformation(
-                        *tuple(c.transformation))
-                # ufo identifier is skipped
-                components.append(component)
-            # guidelines
-            guidelines = layer.guidelines
-            for g_ in g.guidelines:
-                guideline = Guideline(g_.x or 0, g_.y or 0, g_.angle or 0)
-                if g_.name:
-                    guideline.name = g_.name
-                # ufo color and identifier are skipped
-                guidelines.append(guideline)
-            # paths
-            paths = layer.paths
-            for c in self.unstructure(g.contours):
-                pts = c.pop("points")
-                for p in pts:
-                    name = p.pop("name", None)
-                    ident = p.pop("identifier", None)
-                    if name or ident:
-                        p["extraData"] = d = {}
-                        if name:
-                            d["name"] = name
-                        if ident:
-                            d["id"] = ident
-                while pts[-1]["type"] is None:
-                    pts.insert(0, pts.pop())
-                c["points"] = pts
-                ident = c.pop("identifier", None)
-                if ident:
-                    c["id"] = ident
-                path = self.structure(c, Path)
-                paths.append(path)
-            glyph._lastModified = None
+
+                # We only need one layer for the master as UFOs are single
+                # masters. Different layers from the UFO are appended under the
+                # same master layer with a "name" attribute.
+                g = ufo_layer[glyph_name]
+                if ufo_layer.name == "public.default":
+                    layer = glyph.layerForMaster(None)
+                else:
+                    master_layer = glyph.layerForMaster(None)
+                    layer = Layer(
+                        masterName=master_layer.masterName, name=ufo_layer.name
+                    )
+                    glyph.layers.append(layer)
+
+                # Use first Unicode value we find, unless already set.
+                if g.unicodes and not glyph.unicodes:
+                    glyph.unicodes = [f"{uv:04X}" for uv in g.unicodes]
+
+                layer.width = g.width
+                layer.height = g.height
+                lib = g.lib
+                vertOrigin = lib.pop("public.verticalOrigin", None)
+                if vertOrigin:
+                    layer.yOrigin = vertOrigin
+                if lib:
+                    layer._extraData = lib
+
+                # anchors
+                anchors = layer.anchors
+                for a in g.anchors:
+                    if not a.name:
+                        continue
+                    anchors[a.name] = Anchor(a.x or 0, a.y or 0)
+                    # ufo color and identifier are skipped
+                # components
+                components = layer.components
+                for c in g.components:
+                    component = Component(c.baseGlyph)
+                    if c.transformation:
+                        component.transformation = Transformation(
+                            *tuple(c.transformation)
+                        )
+                    # ufo identifier is skipped
+                    components.append(component)
+                # guidelines
+                guidelines = layer.guidelines
+                for g_ in g.guidelines:
+                    guideline = Guideline(g_.x or 0, g_.y or 0, g_.angle or 0)
+                    if g_.name:
+                        guideline.name = g_.name
+                    # ufo color and identifier are skipped
+                    guidelines.append(guideline)
+                # paths
+                paths = layer.paths
+                for c in self.unstructure(g.contours):
+                    pts = c.pop("points")
+                    for p in pts:
+                        name = p.pop("name", None)
+                        ident = p.pop("identifier", None)
+                        if name or ident:
+                            p["extraData"] = d = {}
+                            if name:
+                                d["name"] = name
+                            if ident:
+                                d["id"] = ident
+                    while pts[-1]["type"] is None:
+                        pts.insert(0, pts.pop())
+                    c["points"] = pts
+                    ident = c.pop("identifier", None)
+                    if ident:
+                        c["id"] = ident
+                    path = self.structure(c, Path)
+                    paths.append(path)
+                glyph._lastModified = None
         return font
 
     def save(self, font, path):
